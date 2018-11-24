5dc46a01ee4ef8997c79b353b47bc69fb554f6c2
Dana Palmer <roiai4@hotmail.com>
2018-11-24 20:31:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,3 +59,18 @@ class Student
     DB[:conn].execute(sql)
   end
 end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+end
+
