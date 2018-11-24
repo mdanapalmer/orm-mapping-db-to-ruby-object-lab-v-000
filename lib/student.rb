246eced6e4c0d6d5c60651f916a00a686bf1d36f
Dana Palmer <roiai4@hotmail.com>
2018-11-24 20:48:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -110,5 +110,16 @@ class Student
       self.new_from_db(row)
     end.first
   end
-  
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?;
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
