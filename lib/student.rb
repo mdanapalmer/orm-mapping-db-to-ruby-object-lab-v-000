9ad2f2e8577a5dd7b2726b1aa82acb491bda46d8
Dana Palmer <roiai4@hotmail.com>
2018-11-24 20:47:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -102,9 +102,10 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      LIMIT 1
+      WHERE grade = 10
+      LIMIT 1;
     SQL
- 
+
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
