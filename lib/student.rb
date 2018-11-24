20ff174e63f924e28885b23fd42a80668b6c57cd
Dana Palmer <roiai4@hotmail.com>
2018-11-24 20:34:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,11 +62,11 @@ class Student
   end
 end
 
-  def self.all_students_in_grade_9(grade)
+  def self.all_students_in_grade_9
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = ?
+      WHERE grade = 9
     SQL
 
     DB[:conn].execute(sql, grade).map do |row|
